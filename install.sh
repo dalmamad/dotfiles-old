e33c96d1adfd3c6952ed27980576969f4eba5a16
dalmamad <dal.mohamad97@gmail.com>
2023-04-22 00:17:21 +0330

    install file

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
-sudo pacman -S git stow neovim xclip picom rofi polybar lazygit ranger alacritty nodejs npm feh maim ripgrep xdotool xcape
+sudo pacman -S git stow neovim xclip picom rofi polybar lazygit ranger alacritty nodejs npm feh maim ripgrep xdotool xcape blueberry flameshot conky xss-lock 
+sudo pacman -S redis docker docker-compose
 yay -S ttf-hack-nerd sct
 
-git clone https://github.com/dalmamad/dotfiles.git ~/.dotfiles
+sudo pacman -S postgresql
+sudo -u postgres initdb -D /var/lib/postgres/data
 
-sudo rm -r ~/.config/i3/ ~/.config/lazygit/ ~/.config/rofi/ ~/.bashrc
-stow -d ~/.dotfiles -t ~/ alacritty i3 lazygit nvim picom polybar ranger rofi xprofile bash
+sudo pacman -S mpv 
+yay -S librewolf-bin
+
+sudo systemctl enable mongodb.service
+sudo systemctl start mongodb.service
+sudo systemctl enable postgresql.service 
+sudo systemctl start postgresql.service 
+sudo systemctl enable redis.service 
+sudo systemctl start redis.service 
+sudo systemctl enable docker.service 
+sudo systemctl start docker.service 
+
+# sudo rm -r ~/.config/i3/ ~/.config/lazygit/ ~/.config/rofi/ ~/.bashrc
+# git clone https://github.com/dalmamad/dotfiles.git ~/.dotfiles
+
+stow alacritty i3 lazygit nvim picom polybar ranger rofi xprofile bash mpv
